c4e71d106f3095516c0943bb7406c09bf36a2ef3
Nicolas <nicolaaaasxd@gmail.com>
2026-05-21 09:26:51 -0300

    zoom 19

--- app/mapa/page.jsx
+++ app/mapa/page.jsx
@@ -107,7 +107,7 @@ export default function MapPage() {
             <CampusMap
               species={species}
               center={[-3.823328842842737, -38.48170791235997]} // escola
-              zoom={20}
+              zoom={19}
               onViewDetails={(sp) => {
                 window.location.href = `/?view=species&id=${sp.id}`;
               }}
