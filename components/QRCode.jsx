078d4075538b6aa1051fc2fa4ad7ed84b4ae03e2
Nicolas <nicolaaaasxd@gmail.com>
2026-06-13 21:09:15 -0300

    abacaxi

--- components/QRCode.jsx
+++ components/QRCode.jsx
@@ -13,6 +13,7 @@ const especies = [
   { id: 7, nome: "Palmeira de manila", url: "https://herancaverde.vercel.app/especies/7c47aae9-b7c3-40b9-b926-855b79675dcf" },
   { id: 8, nome: "Ixora casei", url: "https://herancaverde.vercel.app/especies/feb1a99e-fd11-4ff4-bd2a-5f5f5f5da538" },
   { id: 9, nome: "Buxinho", url: "https://herancaverde.vercel.app/especies/b7c307c5-8ab8-4287-aa35-31a33451060b" },
+  { id: 10, nome: "abacaxi", url: "https://herancaverde.vercel.app/especies/84ef7fa1-3d88-4b0b-b633-63f9b5e0958e" },
 ];
 
 export default function MultiQR() {
